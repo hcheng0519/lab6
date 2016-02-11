8da4e6053397677f07294b2f9d43eb961ccfe214
Helen Cheng <hcheng0519@gmail.com>
2016-02-11 19:33:04 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,6 +27,17 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	$.get("/project/" + idNumber, addProjectResults);
+	console.log("Getting URL for: /project/" + idNumber);
+}
+
+function addProjectResults(result) {
+	console.log(result);
+	var idNumber = result['id'];
+	var projectHTML = '<img src="' + result['image'] + '" class = detailsImage">' 
+	+ '<p>' + result['summary'] + '</p>' + '<p><small>' + result['date'] + '</small></p>';
+	$("#project" + idNumber + " .details").html(projectHTML);
 }
 
 /*
@@ -34,5 +45,18 @@ function addProjectDetails(e) {
  * and apply it
  */
 function randomizeColors(e) {
+	e.preventDefault();
 	console.log("User clicked on color button");
+	$.get("/palette", randomColors);
+}
+
+function randomColors(result){
+	var hex = result['colors'];
+	var colors = hex['hex'];
+
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
